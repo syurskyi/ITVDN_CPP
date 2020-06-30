9468ba3a4f29093c52c77d3e8949e50cf2cd9444
Grey <sergejyurskyj@yahoo.com>
2020-06-30 16:52:23 -0700

    Committed 011_Auto

---
+++ CPP_Starter/003_DataTypes GR/011_Auto/011_Auto.cpp
@@ -0,0 +1,22 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// В стандарте С++11 появилось новое средство позволяющее компилятору определять тип переменной
+
+	auto a = 1; // переменная a типа int
+
+	auto b = 1.1; // переменная b типа double
+
+	auto c = 'A'; // переменная c типа char
+
+    cout << a << endl << b << endl << c << endl;
+
+	// Задержка
+	cin.get(); 
+    return 0;
+}
+
