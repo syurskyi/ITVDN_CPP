e37ac84470d2ced9a399def8bc6c1ba5c4a2c107
Grey <sergejyurskyj@yahoo.com>
2020-06-30 16:13:06 -0700

    Committed 002_SimpleVariable

---
+++ CPP_Starter/003_DataTypes GR/002_SimpleVariable/002_SimpleVariable.cpp
@@ -0,0 +1,22 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// На 12-й строке мы создаем переменную типа int и присваиваем ей значение 2
+	int a;		// 0000 0000  0000 0000  0000 0000  0000 0010 
+
+	char month = 12;
+    cout << "month: " << month << endl;
+
+
+	month = 13;
+	cout << "month: " << month << endl;
+
+	// Задержка
+	cin.get();
+    return 0;
+}
+
