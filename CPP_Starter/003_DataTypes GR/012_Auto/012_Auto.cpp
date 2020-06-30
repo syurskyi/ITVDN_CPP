737ccbaeb32b2361c3cd1960f1abbae07dc6c5e6
Grey <sergejyurskyj@yahoo.com>
2020-06-30 16:55:02 -0700

    Committed 012_Auto

---
+++ CPP_Starter/003_DataTypes GR/012_Auto/012_Auto.cpp
@@ -0,0 +1,24 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	auto a = 0.0; //переменная а имеет тип double, так как 0.0 - это double 
+
+	double b = 0; // 0 автоматически преобразуется в 0.0
+	  
+	auto c = 0; // переменная с имеет тип int, поскольку 0 - это int
+
+	auto d = 1, e = 2;	// Множественное обьявление
+	//auto d;
+	//d = 4;
+
+    cout << a << endl << b << endl << c << endl << d << endl << e << endl;
+	
+	// Задержка
+	cin.get();
+    return 0;
+}
+
