ec2e3bebde58c9e51355cbfeaf3942bf3d2db645
Grey <sergejyurskyj@yahoo.com>
2020-06-30 16:57:41 -0700

    Committed 013_Dynamic

---
+++ CPP_Starter/003_DataTypes GR/013_Dynamic/013_Dynamic.cpp
@@ -0,0 +1,19 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int width = 10, height = 5;
+
+	// Динамическая инициализация переменной
+	int perimetr = width * height;
+
+    cout << perimetr << endl;
+
+	// Задержка 
+	cin.get();
+    return 0;
+}
+
