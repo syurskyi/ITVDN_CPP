0c50d9c366ab7153078ff5aaf3a0db130ce1146a
Grey <sergejyurskyj@yahoo.com>
2020-06-30 15:54:46 -0700

    Committed DefaultTypes

---
+++ CPP_Starter/002_Storedlnformation/DefaultTypes/DefaultTypes.cpp
@@ -0,0 +1,20 @@
+
+// DefaultTypes.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 5;
+	char b = 'B';
+	double c;
+
+	cout << b;
+
+    return 0;
+}
+
