1966efc30d611d43cc07a4d94e4bac49e6cf542e
Grey <sergejyurskyj@yahoo.com>
2020-07-05 14:40:14 -0700

    Committed ITVDN - C++ Starter 008 - Pointers, 002_Pointer

---
+++ CPP_Starter/008_Pointers/002_Pointer/002Pointer.cpp
@@ -0,0 +1,17 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int age = 25;
+
+	int *p_age = &age;
+
+	cout << "p_age is at: 0x" << p_age << endl;
+
+	system("pause");
+	return 0;
+}
+
