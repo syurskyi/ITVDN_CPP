b8f70007df7d1d21286c20e613bc82766c943c0b
Grey <sergejyurskyj@yahoo.com>
2020-07-05 15:27:37 -0700

    Committed ITVDN - C++ Starter 010 - Array 015Array

---
+++ CPP_Starter/010_Array/015_Array/015Array.cpp
@@ -0,0 +1,31 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Динамическое выделение памяти. С помощью оператора new можно выделять необходимое количество памяти.
+
+	int *pointer = new int[3];
+
+	// Способы заполнить динамический массив
+	// 1-й
+	pointer[0] = 1;
+	pointer[1] = 2;
+	pointer[2] = 3;
+
+	//2-й
+	*pointer = 1;
+	*(pointer + 1) = 2;
+	*(pointer + 2) = 3;
+
+	cout << "Value : " << *pointer			<< "; Adress : " << pointer << endl;
+	cout << "Value : " << *(pointer + 1)	<< "; Adress : " << pointer + 1 << endl;
+	cout << "Value : " << *(pointer + 2)	<< "; Adress : " << pointer + 2 << endl;
+
+
+	system("pause");
+	return 0;
+}
+
