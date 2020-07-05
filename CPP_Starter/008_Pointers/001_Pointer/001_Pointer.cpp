a973ed405615d0dcf3ac92e533c407444f5417f5
Grey <sergejyurskyj@yahoo.com>
2020-07-05 14:38:48 -0700

    Committed ITVDN - C++ Starter 008 - Pointers, 013_Functions

---
+++ CPP_Starter/008_Pointers/001_Pointer/001_Pointer.cpp
@@ -0,0 +1,19 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int age = 18;
+	double Pi = 3.14;
+
+	cout << "Address of variable age is: 0x" << &age << endl;
+
+	cout << "Address of variable Pi is: 0x" << &Pi << endl;
+
+	system("pause");
+    return 0;
+}
+
