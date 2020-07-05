a2ff2c60c2617f7fbbc800b7c794d3cdf83292d5
Grey <sergejyurskyj@yahoo.com>
2020-07-05 14:49:12 -0700

    Committed ITVDN - C++ Starter 008 - Pointers, 011pointerconstant

---
+++ CPP_Starter/008_Pointers/011_pointer_constant/011pointerconstant.cpp
@@ -0,0 +1,24 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 10;
+
+	int *const pA = &a;
+
+	// Данные на которые указывает указатель изменять допустимо.
+	*pA = 11;
+
+	cout << "Address : " << pA << ", value = " << *pA << endl;
+
+	int b = 12;
+
+	// Нельзя изменить адрес, хранящийся в указателе.
+	// pA = &b;
+
+	system("pause");
+	return 0;
+}
