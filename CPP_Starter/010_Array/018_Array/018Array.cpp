d2b27c794434d9390232f1b549a925a1f3506ebc
Grey <sergejyurskyj@yahoo.com>
2020-07-05 15:30:00 -0700

    Committed ITVDN - C++ Starter 010 - Array 018Array

---
+++ CPP_Starter/010_Array/018_Array/018Array.cpp
@@ -0,0 +1,23 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+// Если аргументом функции является массив, его можно обьявить тремя способами
+
+void func1(int *p_arr)	// Указатель
+{ 
+}
+
+void func2(int arr[10])	// Массив фиксированного размера
+{ 
+}
+
+void func3(int arr[])	// Массив не определенного размера
+{
+}
+
+int main()
+{
+	system("pause");
+	return 0;
+}
