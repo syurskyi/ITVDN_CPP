b112552ff6cdd42b279ab4ab103545e97173e3f8
Grey <sergejyurskyj@yahoo.com>
2020-07-05 14:53:29 -0700

    Committed ITVDN - C++ Starter 008 - Pointers, 014function

---
+++ CPP_Starter/008_Pointers/014_function/014function.cpp
@@ -0,0 +1,41 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+void ChangeValue(int a, int b);
+void ChangeValueWithPointers(int *a, int *b);
+
+int main()
+{
+	int a = 10, b = 15;
+
+	cout << "Before call functions: a = " << a << ", b = " << b << endl;
+
+	ChangeValue(a, b);
+
+	cout << "After call ChangeValue: a = " << a << ", b = " << b << endl;
+
+	ChangeValueWithPointers(&a, &b);
+
+	cout << "After call ChangeValueWithPointers: a = " << a << ", b = " << b << endl;
+
+	system("pause");
+	return 0;
+}
+
+void ChangeValue(int a, int b)
+{
+	int tmp = a;
+
+	a = b;
+	b = tmp;
+}
+
+void ChangeValueWithPointers(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
