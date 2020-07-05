bb9ea179d89e393ea09e50df07c45f12fdaca324
Grey <sergejyurskyj@yahoo.com>
2020-07-05 15:15:38 -0700

    Committed ITVDN - C++ Starter 010 - Array 004Array

---
+++ CPP_Starter/010_Array/004_Array/004Array.cpp
@@ -0,0 +1,26 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int size = 10;
+	int arr[size];
+
+	// Заполнение массива
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = i * 2;
+	}
+
+	// Вывод на экран значений массива
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << endl;
+	}
+
+	system("pause");
+	return 0;
+}
+
