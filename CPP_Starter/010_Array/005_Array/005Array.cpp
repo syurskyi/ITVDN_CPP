f50c957ac275df196a4a01721b3cc32d79666062
Grey <sergejyurskyj@yahoo.com>
2020-07-05 15:17:36 -0700

    Committed ITVDN - C++ Starter 010 - Array 005Array

---
+++ CPP_Starter/010_Array/005_Array/005Array.cpp
@@ -0,0 +1,19 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+	// Вывод на экран значений массива
+	for (int i = 0; i < (sizeof(arr) / sizeof(arr[0])); i++)
+	{
+		cout << arr[i] << endl;
+	}
+
+	system("pause");
+	return 0;
+}
+
