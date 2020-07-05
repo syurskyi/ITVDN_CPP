3dc29dbba5c2daff79307c1c56fb76e31ee9cfac
Grey <sergejyurskyj@yahoo.com>
2020-07-05 15:20:58 -0700

    Committed ITVDN - C++ Starter 010 - Array 009Array

---
+++ CPP_Starter/010_Array/009_Array/009Array.cpp
@@ -0,0 +1,21 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int vector[1];
+	vector[0] = 10;
+	cout << vector[0] << endl;
+
+	int matrix[1][1];
+	matrix[0][0] = 100;
+	cout << matrix[0][0] << endl;
+
+	system("pause");
+	return 0;
+}
+
+
+
