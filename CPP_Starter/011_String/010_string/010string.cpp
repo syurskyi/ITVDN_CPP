f60be571f55f240746e081c404761862e66f2051
Grey <sergejyurskyj@yahoo.com>
2020-07-05 15:44:56 -0700

    Committed ITVDN - C++ Starter 011 - String 009string

---
+++ CPP_Starter/011_String/010_string/010string.cpp
@@ -0,0 +1,20 @@
+﻿#include "stdafx.h"
+#include <iostream>
+// Библиотека обработки строк в стиле СИ
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	char str1[20] = "Hello";
+	char str2[] = " world!";
+	// функция производит конкатенацию строк, записывая второй параметр в первый.
+	strcat(str1, str2);
+
+	cout << str1 << endl;
+
+	system("pause");
+	return 0;
+}
+
---
+++ CPP_Starter/011_String/011_string/011string.cpp
@@ -0,0 +1,34 @@
+﻿#include "stdafx.h"
+#include <iostream>
+// Библиотека обработки строк в стиле СИ
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	char str1[] = "Hello";
+	char str2[] = " world!";
+	char str3[] = "Hello";
+
+	// функция производит сравнение строк.
+	// Возвращает 0, если строки совпадают, отрицательное значение, если первая меньше второй, и положительное значение, если первая больше второй.
+	int res = strcmp(str1, str2);
+
+	switch (res)
+	{
+	case 0:
+		cout << "The string is equal!" << endl;
+		break;
+	case -1:
+		cout << "First string is less than second!" << endl;
+		break;
+	case 1:
+		cout << "First string is more than second!" << endl;
+		break;	
+	}
+
+	system("pause");
+	return 0;
+}
+
