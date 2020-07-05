5f192c91080dbdbd7cac87eb31c4c95fd9854bc1
Grey <sergejyurskyj@yahoo.com>
2020-07-05 14:41:32 -0700

    Committed ITVDN - C++ Starter 008 - Pointers, 003Pointer

---
+++ CPP_Starter/008_Pointers/003__Pointer/003Pointer.cpp
@@ -0,0 +1,24 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	int b = 15;
+
+	int *pointer = &a;
+
+	cout << "*pointer = " << *pointer << endl;
+
+	pointer = &b;
+
+	cout << "*pointer = " << *pointer << endl;
+
+	// Всегда инициализируйте указатели. Непроинициализированные указатели являются источниками ошибок и аварийного завершения программы.
+	int *p_void = NULL; // нулевой указатель
+
+	system("pause");
+	return 0;
+}
