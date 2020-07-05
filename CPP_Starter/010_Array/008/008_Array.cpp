65aaadf8ff9f0b0ca51c7f8484355952da9d11eb
Grey <sergejyurskyj@yahoo.com>
2020-07-05 15:20:10 -0700

    Committed ITVDN - C++ Starter 010 - Array 008_Array

---
+++ CPP_Starter/010_Array/008/008_Array.cpp
@@ -0,0 +1,28 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// На 9-й строке создаем двумерный массив с именем  arr типа int размерностью 3 на 3 элемента.
+	int arr[3][3] = {
+						{ 1, 2, 3 },
+						{ 4, 5, 6 },
+						{ 7, 8, 9 }
+					};
+
+	// Выводим значения элементов массива в консоль
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	system("pause");
+	return 0;
+}
+
