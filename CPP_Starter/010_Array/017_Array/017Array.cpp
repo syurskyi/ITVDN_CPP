557bcb280a71a5af484c595db638eee0b08dcc81
Grey <sergejyurskyj@yahoo.com>
2020-07-05 15:29:05 -0700

    Committed ITVDN - C++ Starter 010 - Array 017Array

---
+++ CPP_Starter/010_Array/017_Array/017Array.cpp
@@ -0,0 +1,35 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// на 9-й строке создаем массив указателей размерностью в 3 элемента
+	int *arr_pointers[3];
+
+	int x = 100, y = 200, z = 300;
+
+	*arr_pointers = &x;
+	*(arr_pointers + 1) = &y;
+	*(arr_pointers + 2) = &z;
+	cout << &x << endl;
+	// Выводим значения массива
+	for (int i = 0; i < 3; i++)
+	{
+		cout << i + 1 << " value in array = " << *(arr_pointers + i) << endl;
+	}
+
+	cout << "--------------------------------------------------" << endl;
+
+	// Выводим значения хранящиеся по адресам находящимся в массиве
+	for (int i = 0; i < 3; i++)
+	{
+		cout << i + 1 << " value = " << *(*(arr_pointers + i)) << endl;
+	}
+
+
+	system("pause");
+	return 0;
+}
+
