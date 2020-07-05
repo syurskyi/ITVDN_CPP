35a4b75c3d900286aeb7d1d31ee86bd29044723c
Grey <sergejyurskyj@yahoo.com>
2020-07-05 15:35:30 -0700

    Committed ITVDN - C++ Starter 011 - String 002string

---
+++ CPP_Starter/011_String/001_string/001string.cpp
@@ -0,0 +1,21 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	// Это не строка
+	char first_string[12] = { 'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '.' };
+
+	// Это строка
+	char second_string[12] = { 'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\0' };
+
+//	cout << "Second_string length = " << strlen(second_string) << endl;
+	cout << second_string << endl;
+
+	 cout << first_string << endl; // Раскомментировать!
+
+	system("pause");
+    return 0;
+}
+
---
+++ CPP_Starter/011_String/002_string/002string.cpp
@@ -0,0 +1,20 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	// Компилятор разбивает строку на массив символов
+	char str1[16] = "I write in C++!";
+
+	cout << str1 << endl;
+
+	// Компилятор сам подсчитывает количество символов в строке и инициализирует массив
+	char str2[] = "And I like it!";
+
+	cout << str2 << endl;
+
+	system("pause");
+	return 0;
+}
+
