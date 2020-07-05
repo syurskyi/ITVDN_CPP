18735c9fcb3675aeebfbfe7b5da0619d8e284ba8
Grey <sergejyurskyj@yahoo.com>
2020-07-05 15:25:06 -0700

    Committed ITVDN - C++ Starter 010 - Array 012Array

---
+++ CPP_Starter/010_Array/012_Array/012Array.cpp
@@ -0,0 +1,24 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Создаем массив с именем arr типа int размерностью 3 элемента
+	int arr[3];
+
+	arr[0] = 10;
+	arr[1] = 20;
+	arr[2] = 30;
+
+	// Выводим значение хранящееся в переменной arr
+	cout << "Variable arr contains - " << arr << endl;
+
+	// Получаем адресс переменной arr и выводим его в консоль
+	cout << "Address of variable arr - " << &arr << endl;
+
+	system("pause");
+	return 0;
+}
+
