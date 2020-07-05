90805097cbac754889eb696028cf382974d04d27
Grey <sergejyurskyj@yahoo.com>
2020-07-05 15:30:39 -0700

    Committed ITVDN - C++ Starter 010 - Array 019Array

---
+++ CPP_Starter/010_Array/019_Array/019Array.cpp
@@ -0,0 +1,36 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int GetSum(int *arr, int count);
+
+int main()
+{
+	int size = 50;
+	int *arr = new int[size];
+
+	for (int i = 0; i < 50; i++)
+	{
+		*(arr + i) = rand();
+	}
+
+	int sum = GetSum(arr, size);
+
+	cout << "Total sum of the array equals = " << sum << endl;
+
+	system("pause");
+	return 0;
+}
+
+int GetSum(int *arr, int count) 
+{
+	int total = 0;
+	for (int i = 0; i < count; i++)
+	{
+		total += *(arr + i);
+	}
+
+	return total;
+}
+
