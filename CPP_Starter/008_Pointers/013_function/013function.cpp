c4f6fc7731f07cc0dc78d5525b6ead7fc8366b33
Grey <sergejyurskyj@yahoo.com>
2020-07-05 14:52:33 -0700

    Committed ITVDN - C++ Starter 008 - Pointers, 013function

---
+++ CPP_Starter/008_Pointers/013_function/013function.cpp
@@ -0,0 +1,30 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+// Константный указатель на константные данные,т.е. ничто не может быть изменено
+void CalcArea(const double* const pPi, const double* const pRadius, double* const pArea) 
+{
+	// проверить указатели перед использованием!
+	if (pPi && pRadius && pArea)
+		*pArea = (*pPi) * (*pRadius) * (*pRadius);
+ }
+
+int main()
+{
+	const double pi = 3.1416;
+	cout << "Enter radius of circle: ";
+
+	double radius = 0;
+	cin >> radius;
+
+	double area = 0;
+
+	CalcArea(&pi, &radius, &area);
+
+	cout << "Area is = " << area << endl;
+
+	system("pause");
+	return 0;
+}
