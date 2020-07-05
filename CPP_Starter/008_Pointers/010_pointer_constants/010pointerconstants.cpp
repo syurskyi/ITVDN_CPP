dd25914bb148c5a4bb03be38389e2374d497808f
Grey <sergejyurskyj@yahoo.com>
2020-07-05 14:48:18 -0700

    Committed ITVDN - C++ Starter 008 - Pointers, 010pointerconstants

---
+++ CPP_Starter/008_Pointers/010_pointer_constants/010pointerconstants.cpp
@@ -0,0 +1,30 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 20;
+
+	// Данные в указателе изменять нельзя.
+	const int *pA = &a;
+
+	cout << pA << endl;
+
+	int b = 30;
+
+	// Можно изменить адрес памяти на который указывает указатель.
+	pA = &b;
+
+	cout << pA << endl;
+
+	// Ошибка компиляции, нельзя изменять данные.
+	//*pA = 40;
+
+	// Ошибка компиляции, нельзя присвоить константу не константе
+	//int *pointer = pA;
+
+	system("pause");
+	return 0;
+}
