6d4e373fd078b4dae41ea5c8f10242998a62cbaf
Grey <sergejyurskyj@yahoo.com>
2020-07-05 14:51:39 -0700

    Committed ITVDN - C++ Starter 008 - Pointers, 012_pointer_constans

---
+++ CPP_Starter/008_Pointers/012_pointer_constans/012pointerconstans.cpp
@@ -0,0 +1,21 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int hour = 12;
+
+	const int *const pHour = &hour;
+
+	// Нельзя изменять данные на которые указывает указатель.
+	 // *pHour = 24;
+	
+	int days = 30;
+	// Нельзя именять адрес на который указывает указатель.
+	// pHour = &days;
+
+	system("pause");
+	return 0;
+}
