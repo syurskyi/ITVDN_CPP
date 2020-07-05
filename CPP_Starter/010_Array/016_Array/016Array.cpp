db211a5e81d2a1c8ac18fa7b74af4ee2563c6961
Grey <sergejyurskyj@yahoo.com>
2020-07-05 15:28:24 -0700

    Committed ITVDN - C++ Starter 010 - Array 016Array

---
+++ CPP_Starter/010_Array/016_Array/016Array.cpp
@@ -0,0 +1,27 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Динамически выделяем массив для 50-ти элементов типа int
+	int *arr = new int[50];
+
+	// Заполняем массив.
+	for (int i = 0; i < 50; i++)
+	{
+		*(arr + i) = (i + 1) * 2;
+	}
+	
+	// Выводим значения элементов массива.
+	for (int i = 0; i < 50; i++)
+	{
+		cout << *(arr + i) << endl;
+	}
+
+
+	system("pause");
+	return 0;
+}
+
